11ddb09adc8a06419522e650d2ba7734d3119aa8
Leonardo Souza <leogsouza@gmail.com>
2019-02-13 23:05:35 -0200

    Change Banner text. Show About page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,6 +9,7 @@ const Banner = () => (
       I'm a software developer with 14+ years of experience. Currently I'm based in São Paulo, Brazil.
       I mostly do Backend Development (Node.js, PHP, Java and Golang) but I do enjoy Frontend (React, Angular and Vue.js) 
       and Mobile (Android, React Native) too.
+      Let's <a href="#about" className="smoothscroll">{" "}scroll down</a>{" "} to learn more <a href="#about" className="smoothscroll">{" "}about me</a>.
       </h3>
       <hr />
       <SocialLinks />
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ import Footer from "../components/Footer";
 const IndexPage = () => (
   <div>
     <Header />
+    <About />
   </div>
 );
 
